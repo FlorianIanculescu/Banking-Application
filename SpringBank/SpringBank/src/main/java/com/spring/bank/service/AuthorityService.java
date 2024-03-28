abfbf2a1f15d22dfce293721b5e82e139f471af2
Florian-Cristinel Ianculescu <florian.ianculescu@gmail.com>
2024-03-28 22:25:23 +0200

    Delete SpringBank directory

--- SpringBank/SpringBank/src/main/java/com/spring/bank/service/AuthorityService.java
+++
@@ -1,8 +0,0 @@
-package com.spring.bank.service;
-
-import com.spring.bank.security.Authority;
-
-public interface AuthorityService {
-
-    void save(Authority theAuthority);
-}
--- SpringBank/SpringBank/src/main/java/com/spring/bank/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.spring.bank.service;
-
-import com.spring.bank.security.User;
-
-public interface UserService {
-
-    void save(User theUser);
-
-}
